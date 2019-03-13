f8b5c1cb8255544ebb517b77f5653752d4cce47e
IgoR-NiK <Igor_2501@mail.ru>
2019-03-14 00:37:41 +0300

    Вложенные представления

---
+++ VueRouterTest/wwwroot/js/components/home-item.vue
@@ -0,0 +1,20 @@
+﻿<template>
+    <div class="flex-container">
+        <page class="flex-item"></page>
+        <page class="flex-item"></page>
+    </div>
+</template>
+
+<script>
+    module.exports = {
+        components: {
+            'page': httpVueLoader('/js/components/page.vue')
+        },
+    };
+</script>
+
+<style>
+    .flex-container {
+        display: flex;
+    }
+</style>
---
+++ VueRouterTest/wwwroot/js/components/page.vue
@@ -0,0 +1,34 @@
+﻿<template>
+    <div>
+        <nav-bar v-bind:items="['SubPage1', 'SubPage2', 'SubPage3']" v-on:change-item="changeItem"></nav-bar>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    var baseItem = httpVueLoader('/js/components/base-item.vue');
+
+    module.exports = {
+        components: {
+            'nav-bar': httpVueLoader('/js/components/nav-bar.vue')
+        },
+        methods: {
+            changeItem: function (item) {
+
+                this.$router.replace({ path: `/page/${item}` });
+            }
+        },
+        router: new VueRouter({
+            mode: 'history',
+            routes: [
+                { path: '/page', component: baseItem },
+              //  { path: '/home', component: homeItem },
+                { path: '/page/:label', component: baseItem }
+            ]
+        })
+    };
+</script>
+
+<style>
+
+</style>
